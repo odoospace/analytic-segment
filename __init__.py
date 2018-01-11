98226e9485111c601d708fb4c9e0039b536d092d
Pedro Gracia <lasarux@MacBook-de-Pedro.local>
2018-01-11 22:22:45 +0200

    + split segments into templates and segments (with campaign info)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 import controllers
 import models
+import base
 import analytic
--- models.py
+++ models.py
@@ -6,10 +6,20 @@ from openerp import models, fields, api
 PATTERN = ['%i', '%02i', '%06i']
 MAX_LEVEL = len(PATTERN)
 
-class analytic_segment(models.Model):
-    _name = 'analytic_segment.segment'
+class analytic_template(models.Model):
+    _name = 'analytic_segment.template'
     _description = 'Segments for analytic porpouse'
+    _rec_name = 'display_name'
+    _order = 'segment'
 
+    @api.depends('name', 'type_id')
+    @api.multi
+    def display_name(self):
+        res = []
+        for obj in self:
+            obj.display_name = '%s [%s]' % (obj.name, obj.type_id.name)
+
+    """
     @api.multi
     def name_get(self):
         res = []
@@ -17,6 +27,7 @@ class analytic_segment(models.Model):
             name = '%s [%s]' % (obj.name, obj.type_id.name)
             res.append((obj.id, name))
         return res
+    """
 
     @api.onchange('type_id')
     def _set_level(self):
@@ -45,7 +56,7 @@ class analytic_segment(models.Model):
     def _get_fullcode(self):
         """recursively get depth level in tree"""
         # segment is empty for virtual ones
-        if self.virtual or not self.type_id:
+        if not self.type_id:
             self.segment = ''
         else:
             fullcode = [self.code]
@@ -68,22 +79,76 @@ class analytic_segment(models.Model):
 
             self.segment = '.'.join(newfullcode)
 
+    def get_childs(self):
+        """return a list with ids of childrens, grandchildrens, etc."""
+        res = []
+        for obj in self.child_ids:
+            res.append(obj)
+            more_childs = obj.get_childs() # recursive!
+            if more_childs:
+                for child in more_childs:
+                    res.append(child)
+        return res
+
+    @api.model
+    def create(self, values):
+        res = super(analytic_template, self).create(values)
+        vals = {
+            'segment_tmpl_id': res.id
+        }
+        # create a segment without campaign related to this template
+        self.env['analytic_segment.segment'].create(vals)
+        return res
+
     # fields
     code = fields.Char(required=True)
+    display_name = fields.Char(compute=display_name, store=True, string="Name")
     name = fields.Char(required=True)
-    type_id = fields.Many2one('analytic_segment.type', required=True) # first segment
+    type_id = fields.Many2one('analytic_segment.type', string='Type', required=True) # first segment
     segment = fields.Char(compute="_get_fullcode", store=True, readonly=True)
     level = fields.Integer(compute="_get_level", store=True, readonly=True)
     level_parent = fields.Integer(related="type_id.level_parent", readonly=True)
     virtual = fields.Boolean(default=False) # we can't use virtual segments
     blocked = fields.Boolean(default=False)
-    parent_id = fields.Many2one('analytic_segment.segment')
-    child_ids = fields.One2many('analytic_segment.segment', 'parent_id')
+    parent_id = fields.Many2one('analytic_segment.template')
+    child_ids = fields.One2many('analytic_segment.template', 'parent_id')
     # one2manys to core models
     analytic_ids = fields.One2many('account.analytic.account', 'segment_id')
 
+    # base
+    user_ids = fields.One2many('analytic_segment.user', 'segment_id')
+    
 
 class analytic_segment(models.Model):
+    # TODO: add campaign to name
+    _name = 'analytic_segment.segment'
+    _description = 'Segments for analytic porpouse'
+    _inherits = {'analytic_segment.template': 'segment_tmpl_id'}
+    _rec_name = 'display_name'
+
+    @api.multi
+    def _is_campaign(self):
+        return self.is_campaign and True or False
+
+    @api.depends('campaign_id', 'segment_tmpl_id')
+    @api.multi
+    def display_name(self):
+        res = []
+        for obj in self:
+            if obj.campaign_id:
+                obj.display_name = '%s <%s>' % (obj.segment_tmpl_id.display_name, obj.campaign_id.name)
+            else:
+                obj.display_name = obj.segment_tmpl_id.display_name
+    
+    display_name = fields.Char(compute=display_name, store=True, string="Name")
+    segment_tmpl_id = fields.Many2one('analytic_segment.template', ondelete="cascade", required=True)
+    campaign_id = fields.Many2one('analytic_segment.campaign')
+    is_campaign = fields.Boolean(compute='_is_campaign')
+    # base
+    #company_ids = fields.Many2many('res.company', 'segment_company_rel')
+
+
+class analytic_segment_type(models.Model):
     _name = 'analytic_segment.type'
     _description = 'Type of segments for analytic porpouse'
 
@@ -91,3 +156,73 @@ class analytic_segment(models.Model):
     name = fields.Char(required=True)
     level_parent = fields.Integer(required=True)
     segment_ids = fields.One2many('analytic_segment.segment', 'type_id')
+
+
+class analytic_segment_user(models.Model):
+    _name = 'analytic_segment.user'
+    _description = 'Collection of segments by user'
+    company_id = fields.Many2one('res.company')
+    segment_id = fields.Many2one('analytic_segment.segment') # with campaign
+    user_id = fields.Many2one('res.users')
+
+
+class analytic_segment_campaign(models.Model):
+    _name = 'analytic_segment.campaign'
+    _description = 'Data for campaigns'
+
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    state = fields.Selection([('open', 'Open'), ('closed', 'Closed'), ('disabled', 'Disabled')])
+    date_start = fields.Date()
+    date_end = fields.Date()
+    # segments
+    segment_top = fields.Many2one('analytic_segment.template')
+    segment_ids = fields.One2many('analytic_segment.segment', 'campaign_id')
+    # base
+    company_id = fields.Many2one('res.company', required=True)
+
+    @api.multi
+    def create(self, values):
+        segment_top = self.env['analytic_segment.template'].browse(values['segment_top'])
+        segments = [segment_top] + segment_top.get_childs()
+        # remove segments
+        for s in self.segment_ids:
+            s.unlink()
+        # create segments
+        s_ids = []
+        for s in segments:
+            vals = {
+                'campaign_id': self.id,
+                'segment_tmpl_id': s.id
+            }
+            n = self.env['analytic_segment.segment'].create(vals)
+            s_ids.append(n.id)
+        values['segment_ids'] = [(6, 0, s_ids)]
+        res_id = super(analytic_segment_campaign, self).create(values)
+        return res_id
+
+    @api.multi
+    def write(self, values, context=None):
+        print 'write', values
+        if values.has_key('segment_top') and self.segment_top.id != values['segment_top']:
+            segment_top = self.env['analytic_segment.template'].browse(values['segment_top'])
+            segments = [segment_top] + segment_top.get_childs()
+            # remove segments
+            for s in self.segment_ids:
+                s.unlink()
+            # create segments
+            s_ids = []
+            for s in segments:
+                vals = {
+                    'campaign_id': self.id,
+                    'segment_tmpl_id': s.id
+                }
+                n = self.env['analytic_segment.segment'].create(vals)
+                s_ids.append(n.id)
+            values['segment_ids'] = [(6, 0, s_ids)]
+        rec = super(analytic_segment_campaign, self).write(values)
+        return rec
+        
+
+    # TODO: override create and write functions in order to generate segments with campaign info
