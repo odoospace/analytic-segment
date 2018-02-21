ed14343e99b65299005c459ba8ec2f6ef731e793
Pedro Gracia <pedro.gracia@impulzia.com>
2018-02-21 17:08:36 +0200

    + add account.py

---
+++ account.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# use analytic_segment with analytic accounts
+
+from openerp import models, fields, api
+
+class account_move(models.Model):
+    _inherit = 'account.move'
+
+    segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
+    segment = fields.Char(related='segment_id.segment', readonly=True)
+
+
+class account_move_line(models.Model):
+    _inherit = 'account.move.line'
+
+    segment_id = fields.Many2one(related='move_id.segment_id', readonly=True, store=True)
+    segment = fields.Char(related='segment_id.segment', readonly=True)
