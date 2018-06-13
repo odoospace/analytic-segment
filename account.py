6a5d941297919bc11403ac0c2913291226d351e3
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-06-13 17:22:53 +0200

    + add domain to models

--- account.py
+++ account.py
@@ -6,24 +6,96 @@ from openerp import models, fields, api
 class account_move(models.Model):
     _inherit = 'account.move'
 
-    segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
+    def _domain_segment(self):
+        # TODO: refactor these 3 functions!!!!
+        if self.env.user.id == 1:
+            # no restrictions
+            domain = []
+        else:
+            segment_tmpl_ids = []
+            segment_ids = self.env.user.segment_ids
+            for s in segment_ids:
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
+                segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
+            virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
+            segment_tmpl_ids += [i.id for i in virtual_segments]
+
+            segment_ids = self.env['analytic_segment.segment'].search([('segment_tmpl_id', 'in', segment_tmpl_ids)])
+            domain = [('id', 'in', [i.id for i in segment_ids])]
+        return domain
+
+    segment_id = fields.Many2one('analytic_segment.segment', domain=_domain_segment) #, required=True)
     segment = fields.Char(related='segment_id.segment', readonly=True)
 
 
 class account_move_line(models.Model):
     _inherit = 'account.move.line'
 
-    segment_id = fields.Many2one(related='move_id.segment_id', readonly=True)
+    def _domain_segment(self):
+        # TODO: refactor these 3 functions!!!!
+        if self.env.user.id == 1:
+            # no restrictions
+            domain = []
+        else:
+            segment_tmpl_ids = []
+            segment_ids = self.env.user.segment_ids
+            for s in segment_ids:
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
+                segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
+            virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
+            segment_tmpl_ids += [i.id for i in virtual_segments]
+
+            segment_ids = self.env['analytic_segment.segment'].search([('segment_tmpl_id', 'in', segment_tmpl_ids)])
+            domain = [('id', 'in', [i.id for i in segment_ids])]
+        return domain
+
+    segment_id = fields.Many2one(related='move_id.segment_id', readonly=True, domain=_domain_segment)
     segment = fields.Char(related='segment_id.segment', readonly=True)
 
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
 
-    segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
+    def _domain_segment(self):
+        # TODO: refactor these 3 functions!!!!
+        if self.env.user.id == 1:
+            # no restrictions
+            domain = []
+        else:
+            segment_tmpl_ids = []
+            segment_ids = self.env.user.segment_ids
+            for s in segment_ids:
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
+                segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
+            virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
+            segment_tmpl_ids += [i.id for i in virtual_segments]
+
+            segment_ids = self.env['analytic_segment.segment'].search([('segment_tmpl_id', 'in', segment_tmpl_ids)])
+            domain = [('id', 'in', [i.id for i in segment_ids])]
+        return domain
+
+    segment_id = fields.Many2one('analytic_segment.segment', domain=_domain_segment) #, required=True)
     segment = fields.Char(related='segment_id.segment', readonly=True)
 
 class account_journal(models.Model):
     _inherit = 'account.journal'
 
-    segment_id = fields.Many2one('analytic_segment.segment') #, required=True)
+    def _domain_segment(self):
+        # TODO: refactor these 3 functions!!!!
+        if self.env.user.id == 1:
+            # no restrictions
+            domain = []
+        else:
+            segment_tmpl_ids = []
+            segment_ids = self.env.user.segment_ids
+            for s in segment_ids:
+                segment_tmpl_ids += [s.segment_id.segment_tmpl_id.id]
+                segment_tmpl_ids += s.segment_id.segment_tmpl_id.get_childs_ids()
+            virtual_segments = self.env['analytic_segment.template'].search([('virtual', '=', True)])
+            segment_tmpl_ids += [i.id for i in virtual_segments]
+
+            segment_ids = self.env['analytic_segment.segment'].search([('segment_tmpl_id', 'in', segment_tmpl_ids)])
+            domain = [('id', 'in', [i.id for i in segment_ids])]
+        return domain
+
+    segment_id = fields.Many2one('analytic_segment.segment', domain=_domain_segment) #, required=True)
     segment = fields.Char(related='segment_id.segment', readonly=True)
