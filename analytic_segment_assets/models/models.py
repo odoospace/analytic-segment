1b2c90bad08dc2fd46045e652e82f5a6e2e6ba0c
Javier Colmeiro <javier.colmeiro@impulzia.com>
2018-11-20 09:51:24 +0100

    + add segment to invoice asset creation and add invoice comments to asset notes

--- analytic_segment_assets/models/models.py
+++ analytic_segment_assets/models/models.py
@@ -136,3 +136,40 @@ class account_asset_depreciation_line(osv.osv):
                 asset.write({'state': 'close'})
         return created_move_ids
 
+
+class account_invoice_line(osv.osv):
+
+    _inherit = 'account.invoice.line'
+    
+    def asset_create(self, cr, uid, lines, context=None):
+        context = context or {}
+        asset_obj = self.pool.get('account.asset.asset')
+        asset_ids = []
+        for line in lines:
+            if line.invoice_id.number:
+                #FORWARDPORT UP TO SAAS-6
+                asset_ids += asset_obj.search(cr, SUPERUSER_ID, [('code', '=', line.invoice_id.number), ('company_id', '=', line.company_id.id)], context=context)
+        asset_obj.write(cr, SUPERUSER_ID, asset_ids, {'active': False})
+        for line in lines:
+            if line.asset_category_id:
+                #FORWARDPORT UP TO SAAS-6
+                sign = -1 if line.invoice_id.type in ("in_refund", 'out_refund') else 1
+                vals = {
+                    'name': line.name,
+                    'code': line.invoice_id.number or False,
+                    'category_id': line.asset_category_id.id,
+                    'purchase_value': sign * line.price_subtotal,
+                    'partner_id': line.invoice_id.partner_id.id,
+                    'company_id': line.invoice_id.company_id.id,
+                    'currency_id': line.invoice_id.currency_id.id,
+                    'purchase_date': line.invoice_id.date_invoice,
+                    'segment_id': line.invoice_id.segment_id.id,
+                    'note': line.invoice_id.comment
+                }
+                changed_vals = asset_obj.onchange_category_id(cr, uid, [], vals['category_id'], context=context)
+                vals.update(changed_vals['value'])
+                asset_id = asset_obj.create(cr, uid, vals, context=context)
+                if line.asset_category_id.open_asset:
+                    asset_obj.validate(cr, uid, [asset_id], context=context)
+        return True
+
