1c863738c72cf1eb6730f1622dbf7197aaf650bc
Javier Colmeiro <javier.colmeiro@impulzia.com>
2019-03-28 08:20:50 +0100

    + add method to unmark move_check in deleted depreciation moves

--- analytic_segment_assets/__openerp__.py
+++ analytic_segment_assets/__openerp__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '8.0.1.10',
+    'version': '8.0.1.11',
     # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'analytic_segment', 'account_asset'],
 
