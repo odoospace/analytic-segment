8829b428c55386ca54ff91e111cc836ae432622e
Javier Colmeiro <javier.colmeiro@impulzia.com>
2019-04-09 18:18:41 +0200

    + add check to prevent reconciliation between segments

--- analytic_segment/models/account.py
+++ analytic_segment/models/account.py
@@ -5,6 +5,9 @@ from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError, Warning
 from openerp.tools import float_compare
 from openerp.osv import osv
+import time
+from openerp import workflow
+
 
 class account_move(models.Model):
     _inherit = 'account.move'
@@ -107,6 +110,246 @@ class account_move_line(models.Model):
     segment = fields.Char(related='segment_id.segment', readonly=True)
     campaign_segment = fields.Boolean(related='move_id.campaign_segment', readonly=True)
     segment_user_id = fields.Many2one('res.users', compute='_segment_user_id', search=_search_segment_user)
+
+
+    #ugly hacks to avoid reconcilation between segments...
+    def reconcile_partial(self, cr, uid, ids, type='auto', context=None, writeoff_acc_id=False, writeoff_period_id=False, writeoff_journal_id=False):
+        move_rec_obj = self.pool.get('account.move.reconcile')
+        merges = []
+        unmerge = []
+        total = 0.0
+        merges_rec = []
+        company_list = []
+        if context is None:
+            context = {}
+
+        segment_id = False
+        campaign = False
+        acc_exception = False
+
+        for line in self.browse(cr, uid, ids, context=context):
+            acc_exception = False
+            if line.account_id.code[0:3] == '759' or uid == 1:
+                acc_exception = True
+
+            if not segment_id and line.segment_id:
+                segment_id = line.segment_id.id
+            if not campaign and line.campaign:
+                campaign = line.campaign
+
+            if not acc_exception:
+                print 'check concil', segment_id, line.segment_id
+                if segment_id != line.segment_id.id:
+                    raise osv.except_osv(_('Warning!'), _('aml: To reconcile the entries segment_id must be the same'))
+                if campaign != line.campaign:
+                    raise osv.except_osv(_('Warning!'), _('aml: To reconcile the entries campaign must be the same'))    
+
+        for line in self.browse(cr, uid, ids, context=context):
+            if company_list and not line.company_id.id in company_list:
+                raise osv.except_osv(_('Warning!'), _('To reconcile the entries company should be the same for all entries.'))
+            company_list.append(line.company_id.id)
+
+        for line in self.browse(cr, uid, ids, context=context):
+            if line.account_id.currency_id:
+                currency_id = line.account_id.currency_id
+            else:
+                currency_id = line.company_id.currency_id
+            if line.reconcile_id:
+                raise osv.except_osv(_('Warning'), _("Journal Item '%s' (id: %s), Move '%s' is already reconciled!") % (line.name, line.id, line.move_id.name))
+            if line.reconcile_partial_id:
+                for line2 in line.reconcile_partial_id.line_partial_ids:
+                    if line2.state != 'valid':
+                        raise osv.except_osv(_('Warning'), _("Journal Item '%s' (id: %s) cannot be used in a reconciliation as it is not balanced!") % (line2.name, line2.id))
+                    if not line2.reconcile_id:
+                        if line2.id not in merges:
+                            merges.append(line2.id)
+                        if line2.account_id.currency_id:
+                            total += line2.amount_currency
+                        else:
+                            total += (line2.debit or 0.0) - (line2.credit or 0.0)
+                merges_rec.append(line.reconcile_partial_id.id)
+            else:
+                unmerge.append(line.id)
+                if line.account_id.currency_id:
+                    total += line.amount_currency
+                else:
+                    total += (line.debit or 0.0) - (line.credit or 0.0)
+        if self.pool.get('res.currency').is_zero(cr, uid, currency_id, total):
+            res = self.reconcile(cr, uid, merges+unmerge, context=context, writeoff_acc_id=writeoff_acc_id, writeoff_period_id=writeoff_period_id, writeoff_journal_id=writeoff_journal_id)
+            return res
+        # marking the lines as reconciled does not change their validity, so there is no need
+        # to revalidate their moves completely.
+        reconcile_context = dict(context, novalidate=True)
+        r_id = move_rec_obj.create(cr, uid, {
+            'type': type,
+            'line_partial_ids': map(lambda x: (4,x,False), merges+unmerge)
+        }, context=reconcile_context)
+        move_rec_obj.reconcile_partial_check(cr, uid, [r_id] + merges_rec, context=reconcile_context)
+        return r_id
+
+    def reconcile(self, cr, uid, ids, type='auto', writeoff_acc_id=False, writeoff_period_id=False, writeoff_journal_id=False, context=None):
+        print '^*/&%$ entro custom reconcile'
+        account_obj = self.pool.get('account.account')
+        move_obj = self.pool.get('account.move')
+        move_rec_obj = self.pool.get('account.move.reconcile')
+        partner_obj = self.pool.get('res.partner')
+        currency_obj = self.pool.get('res.currency')
+        lines = self.browse(cr, uid, ids, context=context)
+        unrec_lines = filter(lambda x: not x['reconcile_id'], lines)
+        credit = debit = 0.0
+        currency = 0.0
+        account_id = False
+        partner_id = False
+        if context is None:
+            context = {}
+        company_list = []
+
+        segment_id = False
+        campaign = False
+        acc_exception = False
+
+        for line in lines:
+            acc_exception = False
+            account = account_obj.browse(cr, uid, line['account_id']['id'], context=context)
+            if account.code[0:3] == '759' or uid == 1:
+                acc_exception = True
+
+            if not segment_id and line['segment_id']:
+                segment_id = line['segment_id']['id']
+            if not campaign and line['campaign']:
+                campaign = line['campaign']
+
+            if not acc_exception:
+                print 'check concil', segment_id, line['segment_id']
+                if segment_id != line['segment_id']['id']:
+                    raise osv.except_osv(_('Warning!'), _('aml: To reconcile the entries segment_id must be the same'))
+                if campaign != line['campaign']:
+                    raise osv.except_osv(_('Warning!'), _('aml: To reconcile the entries campaign must be the same'))
+
+        for line in lines:
+            if company_list and not line.company_id.id in company_list:
+                raise osv.except_osv(_('Warning!'), _('To reconcile the entries company should be the same for all entries.'))
+            company_list.append(line.company_id.id)
+        for line in unrec_lines:
+            if line.state <> 'valid':
+                raise osv.except_osv(_('Error!'),
+                        _('Entry "%s" is not valid !') % line.name)
+            credit += line['credit']
+            debit += line['debit']
+            currency += line['amount_currency'] or 0.0
+            account_id = line['account_id']['id']
+            partner_id = (line['partner_id'] and line['partner_id']['id']) or False
+        writeoff = debit - credit
+
+        # Ifdate_p in context => take this date
+        if context.has_key('date_p') and context['date_p']:
+            date=context['date_p']
+        else:
+            date = time.strftime('%Y-%m-%d')
+
+        cr.execute('SELECT account_id, reconcile_id '\
+                   'FROM account_move_line '\
+                   'WHERE id IN %s '\
+                   'GROUP BY account_id,reconcile_id',
+                   (tuple(ids), ))
+        r = cr.fetchall()
+        #TODO: move this check to a constraint in the account_move_reconcile object
+        if len(r) != 1:
+            raise osv.except_osv(_('Error'), _('Entries are not of the same account or already reconciled ! '))
+        if not unrec_lines:
+            raise osv.except_osv(_('Error!'), _('Entry is already reconciled.'))
+        account = account_obj.browse(cr, uid, account_id, context=context)
+        if not account.reconcile:
+            raise osv.except_osv(_('Error'), _('The account is not defined to be reconciled !'))
+        if r[0][1] != None:
+            raise osv.except_osv(_('Error!'), _('Some entries are already reconciled.'))
+
+        if (not currency_obj.is_zero(cr, uid, account.company_id.currency_id, writeoff)) or \
+           (account.currency_id and (not currency_obj.is_zero(cr, uid, account.currency_id, currency))):
+            if not writeoff_acc_id:
+                raise osv.except_osv(_('Warning!'), _('You have to provide an account for the write off/exchange difference entry.'))
+            if writeoff > 0:
+                debit = writeoff
+                credit = 0.0
+                self_credit = writeoff
+                self_debit = 0.0
+            else:
+                debit = 0.0
+                credit = -writeoff
+                self_credit = 0.0
+                self_debit = -writeoff
+            # If comment exist in context, take it
+            if 'comment' in context and context['comment']:
+                libelle = context['comment']
+            else:
+                libelle = _('Write-Off')
+
+            cur_obj = self.pool.get('res.currency')
+            cur_id = False
+            amount_currency_writeoff = 0.0
+            if context.get('company_currency_id',False) != context.get('currency_id',False):
+                cur_id = context.get('currency_id',False)
+                for line in unrec_lines:
+                    if line.currency_id and line.currency_id.id == context.get('currency_id',False):
+                        amount_currency_writeoff += line.amount_currency
+                    else:
+                        tmp_amount = cur_obj.compute(cr, uid, line.account_id.company_id.currency_id.id, context.get('currency_id',False), abs(line.debit-line.credit), context={'date': line.date})
+                        amount_currency_writeoff += (line.debit > 0) and tmp_amount or -tmp_amount
+
+            writeoff_lines = [
+                (0, 0, {
+                    'name': libelle,
+                    'debit': self_debit,
+                    'credit': self_credit,
+                    'account_id': account_id,
+                    'date': date,
+                    'partner_id': partner_id,
+                    'currency_id': cur_id or (account.currency_id.id or False),
+                    'amount_currency': amount_currency_writeoff and -1 * amount_currency_writeoff or (account.currency_id.id and -1 * currency or 0.0)
+                }),
+                (0, 0, {
+                    'name': libelle,
+                    'debit': debit,
+                    'credit': credit,
+                    'account_id': writeoff_acc_id,
+                    'analytic_account_id': context.get('analytic_id', False),
+                    'date': date,
+                    'partner_id': partner_id,
+                    'currency_id': cur_id or (account.currency_id.id or False),
+                    'amount_currency': amount_currency_writeoff and amount_currency_writeoff or (account.currency_id.id and currency or 0.0)
+                })
+            ]
+
+            writeoff_move_id = move_obj.create(cr, uid, {
+                'period_id': writeoff_period_id,
+                'journal_id': writeoff_journal_id,
+                'date':date,
+                'state': 'draft',
+                'line_id': writeoff_lines
+            })
+
+            writeoff_line_ids = self.search(cr, uid, [('move_id', '=', writeoff_move_id), ('account_id', '=', account_id)])
+            if account_id == writeoff_acc_id:
+                writeoff_line_ids = [writeoff_line_ids[1]]
+            ids += writeoff_line_ids
+
+        # marking the lines as reconciled does not change their validity, so there is no need
+        # to revalidate their moves completely.
+        reconcile_context = dict(context, novalidate=True)
+        r_id = move_rec_obj.create(cr, uid, {'type': type}, context=reconcile_context)
+        self.write(cr, uid, ids, {'reconcile_id': r_id, 'reconcile_partial_id': False}, context=reconcile_context)
+
+        # the id of the move.reconcile is written in the move.line (self) by the create method above
+        # because of the way the line_id are defined: (4, x, False)
+        for id in ids:
+            workflow.trg_trigger(uid, 'account.move.line', id, cr)
+
+        if lines and lines[0]:
+            partner_id = lines[0].partner_id and lines[0].partner_id.id or False
+            if partner_id and not partner_obj.has_something_to_reconcile(cr, uid, partner_id, context=context):
+                partner_obj.mark_as_reconciled(cr, uid, [partner_id], context=context)
+        return r_id
+
 
 
 class account_invoice(models.Model):
