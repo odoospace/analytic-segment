65ed98b746e3ffebbe1cfa3f2bd8909f8d74ef31
Javier Colmeiro <javier.colmeiro@impulzia.com>
2019-04-11 10:46:04 +0200

    + add store and default for related segment_ids

--- analytic_segment/models/account.py
+++ analytic_segment/models/account.py
@@ -110,7 +110,7 @@ class account_move_line(models.Model):
     def _update_segment_id(self):
         self.segment_id = self.move_id.segment_id
 
-    segment_id = fields.Many2one(related='move_id.segment_id', index=True, store=True, readonly=True, domain=_domain_segment)
+    segment_id = fields.Many2one(related='move_id.segment_id', index=True, store=True, readonly=True, domain=_domain_segment, default=2)
     segment = fields.Char(related='segment_id.segment', readonly=True)
     campaign_segment = fields.Boolean(related='move_id.campaign_segment', readonly=True)
     segment_user_id = fields.Many2one('res.users', compute='_segment_user_id', search=_search_segment_user)
