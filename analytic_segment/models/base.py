d79f40f3a0fa0e4a634e704b5e94c93282a66844
Pedro Gracia <pedro.gracia@impulzia.com>
2019-05-07 08:56:57 +0200

    - new filter for segments by user and company

--- analytic_segment/models/base.py
+++ analytic_segment/models/base.py
@@ -16,38 +16,52 @@ class res_users(models.Model):
         else:
             users = [self.env.user]
 
-        for user in users[1:]:
+        for user in users:
             print 'Recalculating user:', user.id, user.name
             ids_to_write = []
             segment_company_all_ids = {}
+            segment_company_open_ids = {}
+            print 'segments:', [i.segment_id.id for i in user.segment_ids]
             for s in user.segment_ids:
+                if not s.comspany_id.id in segment_company_all_ids:
+                    segment_company_all_ids[s.company_id.id] = []
+                segment_company_all_ids[s.company_id.id] += [s.segment_id.id]
+                if s.with_childs:
+                    segment_company_all_ids[s.company_id.id] += s.segment_id.get_childs_ids()
+                    
                 if not s.campaign_id or (s.campaign_id and s.campaign_id.state == 'open'):
-                    if not s.company_id in segment_company_all_ids:
-                        segment_company_all_ids[s.company_id.id] = []
-                    segment_company_all_ids[s.company_id.id] += [s.segment_id.id]
+                    if not s.company_id in segment_company_open_ids:
+                        segment_company_open_ids[s.company_id.id] = []
+                    segment_company_open_ids[s.company_id.id] += [s.segment_id.id]
                     if s.with_childs:
-                        segment_company_all_ids[s.company_id.id] += s.segment_id.get_childs_ids()
+                        segment_company_open_ids[s.company_id.id] += s.segment_id.get_childs_ids()
+            
             # add virtual companies segments
             virtual_segments = self.env['analytic_segment.segment'].search([('virtual', '=', True)])
             for company in segment_company_all_ids.keys():
                 segment_company_all_ids[company] += [i.id for i in virtual_segments]
+                if not company in segment_company_open_ids:
+                    segment_company_open_ids[company] = []
+                segment_company_open_ids[company] += [i.id for i in virtual_segments]
 
             # mark segments with user id
             for segment in segment_company_all_ids.values():
                 ids_to_write += segment
 
             print 1, segment_company_all_ids
-            print 2, ids_to_write
+            print 2, segment_company_open_ids
+            #print 2, ids_to_write
 
-            user_segments = [i.id for i in user.segment_segment_ids]
-            if ids_to_write or user_segments:
-                print 3, user_segments
+            #user_segments = [i.id for i in user.segment_segment_ids]
+            #if ids_to_write or user_segments:
+                #print 3, user_segments
                 #if set(ids_to_write) != set(user_segments):
                     #user.segment_segment_ids = None
                     #user.write({'segment_segment_ids': (6, _, [ids_to_write])})
-                segments = self.env['analytic_segment.segment'].search([('id', 'in', list(set(ids_to_write)))])
-                user.segment_segment_ids = segments
-                user.segment_by_company = json.dumps(segment_company_all_ids)
+            segments = self.env['analytic_segment.segment'].search([('id', 'in', list(set(ids_to_write)))])
+            user.segment_segment_ids = segments
+            user.segment_by_company = json.dumps(segment_company_all_ids)
+            user.segment_by_company_open = json.dumps(segment_company_open_ids)
                 #    print user.login, 'segments updated!', ids_to_write, user_segments
                 #else:
                 #    print user.login, 'segments matches!', ids_to_write, user_segments
@@ -60,6 +74,7 @@ class res_users(models.Model):
 
     segment_segment_ids = fields.Many2many('analytic_segment.segment', 'segment_user_rel', string='Segments segments')
     segment_by_company = fields.Char(default=None)
+    segment_by_company_open = fields.Char(default=None)
     segment_ids = fields.One2many('analytic_segment.user', 'user_id', string='Segments')
 
 
