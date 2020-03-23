8973a1f2ccecfbc1aace6e783f476608e1b6fd77
Javier Colmeiro <javier.colmeiro@impulzia.com>
2020-03-23 17:36:01 +0100

    + deactivate analytic_segment_payment

--- analytic_segment_payment/__manifest__.py
+++ analytic_segment_payment/__manifest__.py
@@ -19,13 +19,13 @@
     'category': 'Uncategorized',
     'version': '8.0.0.1',
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'analytic_segment', 'account_banking_payment_export', 'account_payment'],
+    'depends': ['base', 'account']#, 'analytic_segment', 'account_banking_payment_export', 'account_payment'],
 
     # always loaded
     'data': [
-        'views/templates.xml',
-        'security/security.xml',
-        'wizard/wizard.xml',
+        # 'views/templates.xml',
+        # 'security/security.xml',
+        # 'wizard/wizard.xml',
     ],
     # only loaded in demonstration mode
     #'demo': [
